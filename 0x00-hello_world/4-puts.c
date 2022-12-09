461a6929ac4232e0f6deb68f9a2faee81d8f75b9
OK-CodeClinic <kehindefranklin@gmail.com>
2022-12-09 10:48:32 -0800

    Printing an output(string) using the puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
