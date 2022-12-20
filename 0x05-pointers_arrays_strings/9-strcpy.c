fccfbe0da3287df69c265a6ff81802436ff3c592
OK-CodeClinic <kehindefranklin@gmail.com>
2022-12-20 10:37:28 -0800

    Function that copies a pointed string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string
+ * @dest: where to copy string to
+ * @src: what to be copied
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
