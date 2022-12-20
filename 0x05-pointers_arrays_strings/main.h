e7eeac70462bfa2c9859b022087d038c427d7cb6
OK-CodeClinic <kehindefranklin@gmail.com>
2022-12-20 09:25:44 -0800

    Header file for this project

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+/**
+ * File: main.h
+ * Author: Kehinde Omokungbe
+ * Description: Header files containing prototypes
+ *	used in 0x05-pointers_array_strings projects
+ *
+ */
+
+#include <stdio.h>
+
+int reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char a);
+void _puts(char *str);
+void print_env(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif
+
+
