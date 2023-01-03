4de3b65c61a0d89632ea7a346e56ff64ae7cd7aa
OK-CodeClinic <kehindefranklin@gmail.com>
2023-01-03 08:45:07 -0800

    Sample Test File to test Functions

---
+++ 0x07-pointers_arrays_strings/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		printf("\n");
+	}
+}
+/**
+ * main - Check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char board[8][8] = {
+		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+	};
+	print_chessboard(board);
+	return (0);
+}
