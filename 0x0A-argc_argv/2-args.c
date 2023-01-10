6c4b9645f00edfa4919f94709b2b0b96498a9182
OK-CodeClinic <kehindefranklin@gmail.com>
2023-01-10 04:26:05 -0800

    Program that proints all arguments it recieve

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments it receives.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
