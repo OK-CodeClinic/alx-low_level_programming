d002af44cc56074f640e896ff6cd820fc88aa793
OK-CodeClinic <kehindefranklin@gmail.com>
2023-07-11 18:16:34 -0700

    The listin

--- 0x1E-search_algorithms/listint/free_list.c
+++ 0x1E-search_algorithms/listint/free_list.c
@@ -2,18 +2,18 @@
 #include "../search_algos.h"
 
 /**
- * free_skiplist - Deallocates a singly linked list
+ * free_list - Deallocates a singly linked list
  *
  * @list: Pointer to the linked list to be freed
  */
-void free_skiplist(skiplist_t *list)
+void free_list(listint_t *list)
 {
-	skiplist_t *node;
+	listint_t *node;
 
 	if (list)
 	{
 		node = list->next;
 		free(list);
-		free_skiplist(node);
+		free_list(node);
 	}
 }
